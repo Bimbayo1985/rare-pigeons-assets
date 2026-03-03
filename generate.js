1428367be7264827ecc028ad587908751c07be8b
Bimbayo1985 <yorlov8@gmail.com>
2026-03-03 20:16:24 +0200

    Create generate.js

---
+++ generate.js
@@ -0,0 +1,52 @@
+const axios = require("axios");
+const fs = require("fs");
+
+async function run() {
+
+  const listUrl = "https://raw.githubusercontent.com/Bimbayo1985/rare-pigeons-assets/main/list.json";
+  const listRes = await axios.get(listUrl);
+
+  const assets = Array.isArray(listRes.data)
+    ? listRes.data.map(x => x.asset)
+    : listRes.data.cards.map(x => x.asset);
+
+  const addressMap = {};
+
+  for (const asset of assets) {
+
+    try {
+      const res = await axios.get(`https://xchain.io/api/balances/${asset}`);
+      const holders = res.data;
+
+      holders.forEach(h => {
+        if (parseFloat(h.quantity) > 0) {
+          if (!addressMap[h.address]) {
+            addressMap[h.address] = new Set();
+          }
+          addressMap[h.address].add(asset);
+        }
+      });
+
+    } catch (e) {
+      console.log("Error:", asset);
+    }
+  }
+
+  const results = Object.entries(addressMap).map(([address, set]) => ({
+    address,
+    uniqueCards: set.size
+  }));
+
+  results.sort((a, b) => b.uniqueCards - a.uniqueCards);
+
+  const payload = {
+    totalCards: assets.length,
+    holders: results.slice(0, 100)
+  };
+
+  fs.writeFileSync("leaderboard.json", JSON.stringify(payload, null, 2));
+
+  console.log("Leaderboard generated.");
+}
+
+run();
